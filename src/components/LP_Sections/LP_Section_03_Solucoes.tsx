a9d7087bde8d2afcb156fdf1a991154650a38ab8
JOAO TARRAN <joaotarran@gmail.com>
2023-08-18 16:20:19 -0300

    Solutions Update

--- src/components/LP_Sections/LP_Section_03_Solucoes.tsx
+++ src/components/LP_Sections/LP_Section_03_Solucoes.tsx
@@ -8,6 +8,14 @@ import { motion as m, AnimatePresence } from "framer-motion";
 import { useDispatch, useSelector } from "react-redux";
 import { setActiveSolution } from "../../context/main_context";
 
+// Video Imports
+import VideoEventos from "../../assets/videos/01_Robios_Eventos_Desktop_720p.mp4";
+import VideoVarejo from "../../assets/videos/02_Robios_Varejo_Desktop_QuickCuts_720p.mp4";
+import VideoSaude from "../../assets/videos/03_Robios_Saúde_Desktop_720p.mp4";
+import VideoNavegação from "../../assets/videos/05_Video_Navegacao.mp4";
+import VideoApresentacao from "../../assets/videos/Robios_Apresentacao.mp4";
+import VideoEventosAtendimento from "../../assets/videos/Robios_Evento_Transporte_Atendimento.mp4";
+
 const LP_Section_03_Solucoes = forwardRef(function LP_Section_03_Solucoes(props, ref: any) {
     const dispatch = useDispatch();
     const activeSolution = useSelector((state: any) => state.activeSolution);
@@ -27,39 +35,51 @@ const LP_Section_03_Solucoes = forwardRef(function LP_Section_03_Solucoes(props,
                 )}
                 {activeSolution === "atendimento" && (
                     <m.div initial={{ x: 1000 }} animate={{ x: 0 }} exit={{ x: -1000 }} className="Solution_Card" key={"ClássicosB"}>
+                        <div className="Solution_Video_Fader"></div>
+                        <video className="Solution_Video" src={VideoEventosAtendimento} autoPlay loop muted />
                         <h3 className="Solution_Title">Atendimento</h3>
-                        <p>
+                        <p className="Solution_Description">
                             Os robôs podem interagir com os clientes, fornecendo informações sobre produtos e ajudando a localizar itens específicos
                             na loja. Ele pode até mesmo ajudar os clientes a fazer compras, oferecendo sugestões com base em suas preferências.
                         </p>
+                        <button className="Solution_KnowMore_Button">Mais Detalhes</button>
                     </m.div>
                 )}
                 {activeSolution === "publicidade" && (
                     <m.div initial={{ x: 1000 }} animate={{ x: 0 }} exit={{ x: -1000 }} className="Solution_Card" key={"EspeciaisB"}>
+                        <div className="Solution_Video_Fader"></div>
+                        <video className="Solution_Video" src={VideoEventos} autoPlay loop muted />
                         <h3 className="Solution_Title">Publicidade</h3>
-                        <p>
+                        <p className="Solution_Description">
                             Os robôs podem realizar atividades promocionais, como distribuir brindes e amostras de produtos, fornecer informações
                             sobre os produtos e serviços e até mesmo realizar vendas diretas.
                         </p>
+                        <button className="Solution_KnowMore_Button">Mais Detalhes</button>
                     </m.div>
                 )}
                 {activeSolution === "inspecao" && (
                     <m.div initial={{ x: 1000 }} animate={{ x: 0 }} exit={{ x: -1000 }} className="Solution_Card" key={"KitsB"}>
+                        <div className="Solution_Video_Fader"></div>
+                        <video className="Solution_Video" src={VideoSaude} autoPlay loop muted />
                         <h3 className="Solution_Title">Inspeção</h3>
-                        <p>
+                        <p className="Solution_Description">
                             Os robôs podem ser programados para inspecionar produtos e materiais em busca de defeitos ou irregularidades, usando o
                             leitor de RFID e código de barras para identificar o produto e armazenar informações sobre a inspeção para análise
                             posterior.
                         </p>
+                        <button className="Solution_KnowMore_Button">Mais Detalhes</button>
                     </m.div>
                 )}
                 {activeSolution === "transporte" && (
                     <m.div initial={{ x: 1000 }} animate={{ x: 0 }} exit={{ x: -1000 }} className="Solution_Card" key={"AssinaturasB"}>
+                        <div className="Solution_Video_Fader"></div>
+                        <video className="Solution_Video" src={VideoNavegação} autoPlay loop muted />
                         <h3 className="Solution_Title">Transporte</h3>
-                        <p>
+                        <p className="Solution_Description">
                             Os robôs podem ser programados para entregar materiais e suprimentos em toda a fábrica, usando o leitor de RFID e código
                             de barras para identificar a localização e a quantidade dos materiais a serem entregues.
                         </p>
+                        <button className="Solution_KnowMore_Button">Mais Detalhes</button>
                     </m.div>
                 )}
             </div>
