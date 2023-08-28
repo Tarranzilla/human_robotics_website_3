adaa56d041682c2987a59916b02cccd84e343c65
JOAO TARRAN <joaotarran@gmail.com>
2023-08-28 15:56:48 -0300

    Update

--- src/components/LP_Sections/LP_Section_01_Inicio.tsx
+++ src/components/LP_Sections/LP_Section_01_Inicio.tsx
@@ -1,5 +1,8 @@
 // React Vanilla Imports
-import { forwardRef } from "react";
+import { forwardRef, useState, useEffect } from "react";
+
+// Framer Motion Imports
+import { motion as m, AnimatePresence } from "framer-motion";
 
 import LogoImg from "../../../public/Logo.svg";
 import backgroundImg from "../../assets/cacau_textures/dalle1.png";
@@ -7,13 +10,45 @@ import CardContainer_Experiencias from "../legacy/Carrossel_Experiencias";
 import CardContainer_Parceiros from "../legacy/Carrossel_Parceiros";
 
 const LP_Section_01_Inicio = forwardRef(function LP_Section_01_Inicio(props, ref: any) {
+    const [text, setText] = useState("Agora");
+    const textArray = ["Agora", "Interativo", "Humano"];
+    let index = 0;
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setText(textArray[index]);
+            index = (index + 1) % textArray.length;
+        }, 2000);
+
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <div className="LP_Section LP_Section_01_Inicio" id="LP_Section_1" ref={ref} key={"LP_Section_1"}>
             <div className="carroussel_left_fade top_fade"></div>
             <div className="carroussel_right_fade top_fade"></div>
             <CardContainer_Experiencias />
             <div className="Section_01_Background"></div>
-            <h2 className="Brand_Declaration">O Futuro da Robótica é Agora</h2>
+            <div className="Brand_Declaration">
+                <h2 className="Brand_Declaration">O Futuro da Robótica é</h2>
+
+                {text === "Agora" && (
+                    <m.h2 initial={{ x: 1000 }} animate={{ x: 0 }} exit={{ x: -1000 }} className="Brand_Name">
+                        Agora
+                    </m.h2>
+                )}
+                {text === "Interativo" && (
+                    <m.h2 initial={{ x: 1000 }} animate={{ x: 0 }} exit={{ x: -1000 }} className="Brand_Name">
+                        Interativo
+                    </m.h2>
+                )}
+                {text === "Humano" && (
+                    <m.h2 initial={{ x: 1000 }} animate={{ x: 0 }} exit={{ x: -1000 }} className="Brand_Name">
+                        Humano
+                    </m.h2>
+                )}
+            </div>
+
             <CardContainer_Parceiros />
             <div className="carroussel_left_fade"></div>
             <div className="carroussel_right_fade"></div>
